2d300bdb752ea1241811f76cfcf46205cf730998
Alan Victor <139500847+alanvc11@users.noreply.github.com>
2024-02-26 16:42:29 -0300

    Update usuario.js

--- public/js/usuario.js
+++ public/js/usuario.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Recupera as informações dos usuários do localStorage
+    
     var storedUsers = JSON.parse(localStorage.getItem('usuarios')) || [];
 
-    // Verifica se há informações de usuários
+   
     if (storedUsers.length > 0) {
-        // Cria um elemento de div para conter as informações dos usuários
         var userInfoContainer = document.getElementById('userInfo');
 
         // Itera sobre os usuários armazenados
@@ -12,22 +11,22 @@ document.addEventListener('DOMContentLoaded', function () {
             var userDiv = document.createElement('div');
             userDiv.classList.add('user-info-box');
 
-            // Cria elementos de parágrafo para cada informação do usuário
+            
             var nameParagraph = document.createElement('p');
             var emailParagraph = document.createElement('p');
             var passwordParagraph = document.createElement('p');
 
-            // Adiciona as informações ao texto dos parágrafos
+           
             nameParagraph.textContent = 'Nome: ' + storedUser.nome;
             emailParagraph.textContent = 'Email: ' + storedUser.email;
             passwordParagraph.textContent = 'Senha: ' + storedUser.senha;
 
-            // Adiciona os parágrafos à div do usuário
+           
             userDiv.appendChild(nameParagraph);
             userDiv.appendChild(emailParagraph);
             userDiv.appendChild(passwordParagraph);
 
-            // Cria um botão de excluir
+          
             var deleteButton = document.createElement('button');
             deleteButton.textContent = 'Excluir';
             deleteButton.addEventListener('click', function () {
@@ -38,23 +37,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 localStorage.setItem('usuarios', JSON.stringify(storedUsers));
 
-                // Recarrega a página para refletir as alterações
                 window.location.reload();
             });
 
-            // Adiciona o botão de excluir à div do usuário
+           
             userDiv.appendChild(deleteButton);
 
-            // Adiciona a div do usuário ao container de informações do usuário
+           
             userInfoContainer.appendChild(userDiv);
         });
     }
 
-    // Adiciona o botão "Adicionar Usuário"
+   
     var btnAdicionarUsuario = document.createElement('button');
     btnAdicionarUsuario.textContent = 'Adicionar Usuário';
 
-    // Adiciona o botão "Adicionar Usuário" abaixo das informações do usuário
+  
     document.body.appendChild(btnAdicionarUsuario);
 
     // Adiciona o manipulador de eventos para o botão "Adicionar Usuário"
